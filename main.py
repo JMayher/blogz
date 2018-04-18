1c44225cc092321f118bdb0b6dac86200ac21c39
Jon Mayher <jlmhm5@gmail.com>
2018-04-17 19:19:28 -0500

    finished build-a-blog

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
+import os
+import jinja2
+
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True 
 db = SQLAlchemy(app)
 
@@ -13,32 +16,56 @@ class Blog(db.Model):
     entry = db.Column(db.String(60))
     body = db.Column(db.String(500))
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, entry, body):
+        self.entry=entry
+        self.body=body
+        
 
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/newpost', methods=['GET', 'POST'])
 def index():
+    error = "Please fill in the title"
+    error1 = "Please fill in the body"
+    
+    return render_template("newpost.html", title="Build a Blog!")
+
+@app.route('/blog', methods=['GET','POST'])
+def addpost():
+    error = "Please fill in the title"
+    error1 = "Please fill in the body"
+    blog = Blog.query.all()
+
+    if request.method == 'GET':
+        
+        id = request.args.get('id')
+        if request.args.get('id'):
+            post = Blog.query.get(id)
+        
+            return render_template("singleblog.html", blog=post)
+        else:
+            return render_template("blog.html", title="Build a Blog!", blogs=blog)
+    
 
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
+        
+        blog_name = request.form['entry']
+        entry_name = request.form['body']
+        if blog_name.strip() == "" and entry_name.strip() == "":
+            return render_template("newpost.html", error=error, error1=error1)
+        elif blog_name.strip() == "":
+            return render_template("newpost.html", error=error, entry_name=entry_name)
+        elif entry_name.strip() == "":
+            return render_template("newpost.html", error1=error1, blog_name=blog_name)
+        else:
+           
+            new_blog = Blog(entry=blog_name, body=entry_name)
+            db.session.add(new_blog)
+            db.session.commit()
+            
+            return redirect("/blog?id={0}".format(new_blog.id))
 
-    tasks = Task.query.all()
-    
-    return render_template("todos.html", title="Get It Done!", tasks=tasks)
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    db.session.delete(task)
-    db.session.commit()
-
-    return redirect('/')
+  
 
 if __name__ == '__main__':
+    app.run()
